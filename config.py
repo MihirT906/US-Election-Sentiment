cef02198f7e596bf5c7129e895731451b803ed98
Mihir Thalanki <mihir.thalanki@gmail.com>
2024-12-05 18:46:59 -0500

    Configure pipeline

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
-#SUBREDDIT_NAME = 'USPolitics'
-SUBREDDIT_NAME = 'MassachusettsPolitics'
+#Visualizating sentiment across States
+SUBREDDIT_NAME = 'USPolitics'
+#SUBREDDIT_NAME = 'MassachusettsPolitics'
+#SUBREDDIT_NAME = 'TexasPolitics'
+
+#Visualising sentiment across news channels
+#SUBREDDIT_NAME = 'FoxNews'
+#SUBREDDIT_NAME = 'CNN'
+#SUBREDDIT_NAME = 'BBCNews'
+
+#Visualising sentiment across policies
+#SUBREDDIT_NAME = 'economy'
+#SUBREDDIT_NAME = 'tax'
+#SUBREDDIT_NAME = 'healthcare'
